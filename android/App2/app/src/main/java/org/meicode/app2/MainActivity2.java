c6cb0ccb343a44db762ad46cfd8a00939b6fac62
DiogoTome <114150484+DiogoTome@users.noreply.github.com>
2022-10-06 12:24:39 +0100

    fazer botões e as suas respetivas ações

--- android/App2/app/src/main/java/org/meicode/app2/MainActivity2.java
+++ android/App2/app/src/main/java/org/meicode/app2/MainActivity2.java
@@ -2,7 +2,10 @@ package org.meicode.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void Retroceder (View view) {
+        Intent intent=new Intent( this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    public void Enviar(View view) {
+        TextView tx=findViewById(R.id.textView5);
+                tx.setText("mudou");
     }
 }
