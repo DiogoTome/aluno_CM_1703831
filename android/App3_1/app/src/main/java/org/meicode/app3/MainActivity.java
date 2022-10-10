52483c55e1dedf571d47b4cc19555264cb73abb9
DiogoTome <114150484+DiogoTome@users.noreply.github.com>
2022-10-10 10:41:01 +0100

    botão entrar App3

--- android/App3_1/app/src/main/java/org/meicode/app3/MainActivity.java
+++ android/App3_1/app/src/main/java/org/meicode/app3/MainActivity.java
@@ -2,7 +2,9 @@ package org.meicode.app3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Entrar (View view) {
+        Intent intent=new Intent( this ,MainActivity2.class);
+        startActivity(intent);
+    }
+
+
+
 }
