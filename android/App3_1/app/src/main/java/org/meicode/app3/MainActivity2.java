5e08bf6916d217fa41286a3f002ec6345d67af5c
DiogoTome <114150484+DiogoTome@users.noreply.github.com>
2022-10-13 12:37:29 +0100

    ativar a opção de trocar imagem

--- android/App3_1/app/src/main/java/org/meicode/app3/MainActivity2.java
+++ android/App3_1/app/src/main/java/org/meicode/app3/MainActivity2.java
@@ -2,7 +2,10 @@ package org.meicode.app3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,4 +14,24 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+    public void send4 (View view) {
+        ImageView iv=findViewById(R.id.imageView9);
+        iv.setImageResource(R.mipmap.mickey4);
+    }
+
+    public void send5 (View view) {
+        ImageView iv=findViewById(R.id.imageView9);
+        iv.setImageResource(R.mipmap.mickey5);
+    }
+    public void send6 (View view) {
+        ImageView iv=findViewById(R.id.imageView9);
+        iv.setImageResource(R.mipmap.mickey7);
+    }
+    public void send7 (View view) {
+        ImageView iv=findViewById(R.id.imageView9);
+        iv.setImageResource(R.mipmap.mickey8);
+    }
+
+
+
 }
